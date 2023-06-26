95e24916a0929ce25e89d065c935c396e14eab96
Omkar Phadtare <omkarphadtare321@gmail.com>
2023-06-26 10:57:01 +0530

    first

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/controller/AdminController.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/controller/AdminController.java
@@ -42,7 +42,7 @@ public class AdminController {
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/springproject","root","");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");
 			Statement stmt = con.createStatement();
 			ResultSet rst = stmt.executeQuery("select * from users where username = '"+username+"' and password = '"+ pass+"' ;");
 			if(rst.next()) {
